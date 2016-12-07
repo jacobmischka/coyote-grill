b3ae79a14947e5b935db5f0b31c2d26b1bd4f909
Jacob Mischka <jacobmischka@gmail.com>
2016-12-07 01:16:01 -0600

    Finish new layout, make no-js usable

--- _js/hero.js
+++ _js/hero.js
@@ -1,10 +1,7 @@
 import Flickity from 'flickity';
-import fadeInHeader from './fade-in-header.js';
 
 let flickity = new Flickity('.carousel', {
 	cellSelector: '.carousel-image',
 	wrapAround: true,
 	autoPlay: true
 });
-
-fadeInHeader('.carousel-overlay', 'header.site-header');
--- _js/main.js
+++ _js/main.js
@@ -2,5 +2,6 @@ import './menu.js';
 import './find-us.js';
 import './food.js';
 import './footer.js';
+import './header.js';
 import './hero.js';
 import './home.js';
