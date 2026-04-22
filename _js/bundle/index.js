c31f5e188426a2ef84dcbbe0fd013c9a102df4bc
Jacob Mischka <jacob@mischka.me>
2026-04-22 11:53:07 -0500

    Remove food script entirely
    I was trusting the linter too much to tell me if it's fine, forgot
    there's no types here.

--- _js/bundle/index.js
+++ _js/bundle/index.js
@@ -1,4 +1,3 @@
-import './food.js';
 import './footer.js';
 import './header.js';
 import './hero.js';
@@ -6,5 +5,5 @@ import './hero.js';
 import Highlights from '../svelte-components/highlights/Highlights.html';
 
 new Highlights({
-	target: document.querySelector('#highlights')
+	target: document.querySelector('#highlights'),
 });
