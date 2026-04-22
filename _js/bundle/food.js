b69e56bc39c56c68724b8eac5e2dfac1405834be
Jacob Mischka <jacob@mischka.me>
2026-04-22 11:43:20 -0500

    Don't error in JS now that food section is hidden

--- _js/bundle/food.js
+++ _js/bundle/food.js
@@ -1,14 +1,14 @@
 const imageMenuItemListsSection = document.querySelector('#image-menu-item-lists');
 const tabs = imageMenuItemListsSection.querySelector('.tabs');
 const content = imageMenuItemListsSection.querySelector('.content');
-for(let tabLink of Array.from(tabs.querySelectorAll('.tab-link'))){
+for(let tabLink of Array.from(tabs?.querySelectorAll('.tab-link'))){
 	tabLink.addEventListener('click', event => {
 		event.preventDefault();
 
-		Array.from(content.querySelectorAll('.active')).map(activeGroup => {
+		Array.from(content?.querySelectorAll('.active')).map(activeGroup => {
 			activeGroup.classList.remove('active');
 		});
-		Array.from(tabs.querySelectorAll('.tab-link.active')).map(activeLink => {
+		Array.from(tabs?.querySelectorAll('.tab-link.active')).map(activeLink => {
 			if(activeLink !== event.target)
 				activeLink.classList.remove('active');
 		});
@@ -16,7 +16,7 @@ for(let tabLink of Array.from(tabs.querySelectorAll('.tab-link'))){
 		event.target.classList.toggle('active');
 		if(event.target.classList.contains('active')){
 			let group = event.target.getAttribute('href');
-			content.querySelector(group).classList.add('active');
+			content?.querySelector(group).classList.add('active');
 		}
 	});
 }
